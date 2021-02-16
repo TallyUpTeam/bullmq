58c250e5d3ff189e794548c89fca8a1ada4db47a
Paul Ashdown <paul@tallyup.com>
2021-02-15 17:41:22 -0800

    fix: job.createBulk doesn't obey custom jobId (see issue 189)

--- src/classes/job.ts
+++ src/classes/job.ts
@@ -86,7 +86,14 @@ export class Job<T = any, R = any, N extends string = string> {
     const client = await queue.client;
 
     const jobInstances = jobs.map(
-      job => new Job<T, R, N>(queue, job.name, job.data, job.opts),
+      job =>
+        new Job<T, R, N>(
+          queue,
+          job.name,
+          job.data,
+          job.opts,
+          job.opts && job.opts.jobId,
+        ),
     );
 
     const multi = client.multi();
